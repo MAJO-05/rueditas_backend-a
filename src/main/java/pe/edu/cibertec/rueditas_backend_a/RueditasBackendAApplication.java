5a6c8bdf3c185f1e72c8252e512173a334dc64a6
User <mariajosegaspar95@gmail.com>
2024-09-24 23:53:39 -0500

    Carga inicial del proyecto

---
+++ src/main/java/pe/edu/cibertec/rueditas_backend_a/RueditasBackendAApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_backend_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasBackendAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RueditasBackendAApplication.class, args);
+	}
+
+}
