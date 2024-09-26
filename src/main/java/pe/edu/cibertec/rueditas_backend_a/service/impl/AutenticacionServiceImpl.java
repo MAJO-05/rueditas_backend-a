1ea9f8bfac720723475f453efe5085d2b03da754
User <mariajosegaspar95@gmail.com>
2024-09-25 19:01:57 -0500

    Final del backend

--- src/main/java/pe/edu/cibertec/rueditas_backend_a/service/impl/AutenticacionServiceImpl.java
+++ src/main/java/pe/edu/cibertec/rueditas_backend_a/service/impl/AutenticacionServiceImpl.java
@@ -1,7 +1,53 @@
 package pe.edu.cibertec.rueditas_backend_a.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.rueditas_backend_a.service.AutenticacionService;
+import pe.edu.cibertec.rueditas_backend_a.dto.LoginRequestDTO;
 
-public class AutenticacionServiceImpl {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+@Service
+public class AutenticacionServiceImpl implements AutenticacionService {
 
+    @Autowired
+    ResourceLoader resourceLoader;
+
+    @Override
+    public String[] validarPlaca(LoginRequestDTO loginRequestDTO) throws IOException{
+
+        String[] datosPlaca = null;
+        Resource resource = resourceLoader.getResource("classpath:placas.txt");
+
+        try(BufferedReader br = new BufferedReader(new FileReader(resource.getFile()))){
+
+                String linea;
+                while ((linea = br.readLine()) != null){
+
+                    String[] datos = linea.split(";");
+                    if (loginRequestDTO.numeroPlaca().equals(datos[0])){
+
+                        datosPlaca = new String[5];
+                        datosPlaca[0] = datos[1];
+                        datosPlaca[1] = datos[2];
+                        datosPlaca[2] = datos[3];
+                        datosPlaca[3] = datos[4];
+                        datosPlaca[4] = datos[5];
+                        break;
+                    }
+                }
+        }catch (IOException e) {
+
+            datosPlaca = null;
+            throw new IOException(e);
+
+        }
+
+        return datosPlaca;
+
+    }
 }
